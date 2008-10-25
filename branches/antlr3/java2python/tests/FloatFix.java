fef374a4f9f0c739afea26b6bc36405a8e490887
troy.melhase <troy.melhase@8b63cfe6-5629-0410-b357-210d9a0d789c>
2008-10-25 01:53:21 +0000

    Restart of an antlr3 branch.  Added python-ized versions of Java.g and JavaTreeParser.g , copyright "HABELITZ Software Developments".

---
+++ branches/antlr3/java2python/tests/FloatFix.java
@@ -0,0 +1,10 @@
+import junit.framework.*;
+
+public class FloatFix extends TestCase {
+
+    public void testFloatFix() {
+        float b = 1.2f;
+        Assert.assertEquals(b, 1.2f);
+    }
+
+}
